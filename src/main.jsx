ae7cfd45d880d02e2a9b7f9023eba75fd0277a76
Bashar <basharmohammed946@gmail.com>
2026-07-15 22:23:07 +0300

    تحميل الخطوط والأيقونات محلياً لتحسين الأداء وسرعة التحميل

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@fontsource/ibm-plex-sans-arabic/400.css';
+import '@fontsource/ibm-plex-sans-arabic/500.css';
+import '@fontsource/ibm-plex-sans-arabic/600.css';
+import '@fontsource/ibm-plex-sans-arabic/700.css';
+import 'material-symbols/outlined.css';
 import App from './App.jsx'
 import GlobalErrorBoundary from './shared/components/GlobalErrorBoundary'
 import { QueryClientProvider } from '@tanstack/react-query'
